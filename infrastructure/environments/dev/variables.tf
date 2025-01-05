7dbb57eb454af6c7f914798abc017a4dd84bfcd3
saipraneeth596 <saipraneeth596@gmail.com>
2025-01-05 14:56:08 +0530

    variable definition

--- infrastructure/environments/dev/variables.tf
+++ infrastructure/environments/dev/variables.tf
@@ -4,18 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "twelve_data_api_key" {
-  description = "API key for Twelve Data"
-  type        = string
-  default     = "5b461858dec54d978503f8318cd37392"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
 variable "environment" {
   description = "Deployment environment"
   type        = string
