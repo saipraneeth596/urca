174c6582855d75df1bbc49ab0928aa410ab568d6
saipraneeth596 <saipraneeth596@gmail.com>
2025-01-05 14:57:00 +0530

    remove unused variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,26 +4,8 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
 variable "environment" {
   description = "Deployment environment"
   type        = string
   default     = "dev"
-}
-
-variable "db_username" {
-  description = "Database username"
-  type        = string
-  default     = "dev_user"
-}
-
-variable "db_password" {
-  description = "Database password"
-  type        = string
-  default     = "dev_password"
 }
